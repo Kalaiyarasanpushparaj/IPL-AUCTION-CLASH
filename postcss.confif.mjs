4bac91349c17e59dda406180f403660c26b04c16
Kalaiyarasanpushparaj <your-email@gmail.com>
2026-03-24 16:50:31 +0530

    Normalize line endings to LF

--- postcss.confif.mjs
+++ postcss.confif.mjs
@@ -4,11 +4,4 @@ const config = {
     tailwindcss: {},
   },
 };
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-3afc453108ca096b97586bda8dea437e2c484a0c
 export default config;
