a0303cfe7eb74a8fef6b3532ef7b31ad06286804
KALAIYARASAN P <kalai162617@gmail.com>
2026-04-16 10:57:58 +0530

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-1030607184-6ee62",
-  "appId": "1:562243209414:web:aeee8c3c2f7966ac4cefc7",
-  "apiKey": "AIzaSyD2TKtKM-XSPES-D-H-OjbJ8gNIiRFMopg",
-  "authDomain": "studio-1030607184-6ee62.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "562243209414"
-};
